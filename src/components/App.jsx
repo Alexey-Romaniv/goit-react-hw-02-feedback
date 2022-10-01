9a145419c280ec23b4fa4f71568e865e7b28c583
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2022-10-01 20:47:57 +0300

    did main work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// export const App = () => {
+//   return;
+// };
+import React, { Component } from 'react';
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleChangeValue = option => {
+    // console.log(option);
+    this.setState({ [option]: this.state[option] + 1 });
+  };
+
+  countTotalFeedback = () => {
+    const totalArr = Object.values(this.state);
+    return totalArr.reduce((acc, el) => acc + el, 0);
+  };
+
+  render() {
+    return (
+      <>
+        <p>Please leave feeback</p>
+        {Object.keys(this.state).map(el => (
+          <button
+            type="button"
+            key={el}
+            option={el}
+            onClick={() => this.handleChangeValue(el)}
+          >
+            {el}
+          </button>
+        ))}
+        <p>Statistics</p>
+        <ul>
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Bad: {this.state.bad}</li>
+          <li>Total: {this.countTotalFeedback()}</li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default Feedback;
